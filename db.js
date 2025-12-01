1a0577a31002c70035310b977559d9c3d61ec234
25LP5105 25LP5105 <25LP5105@G302-CC0181.local>
2025-12-01 12:29:33 +0800

    .

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://Beebeg:beebeg1212@cluster0.sfxcwyw.mongodb.net/"
+      "mongodb+srv://Beebeg:beebeg1212@cluster0.8hlrr5h.mongodb.net/"
     );
     console.log("DATABASE connection success");
   } catch (err) {
