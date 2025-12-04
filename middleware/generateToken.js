4e1033ee69f468e84d16036505f26f72b1c97f9e
chinkhuu <42198023+chinkhuu@users.noreply.github.com>
2025-12-05 07:51:11 +0800

    ..k

---
+++ middleware/generateToken.js
@@ -0,0 +1,8 @@
+// const jwt= require ("jsonwebtoken");
+// const JWT_SECRET= "12345";
+
+// const generateToken = (user)=>{
+//     return jwt.sign({user}, JWT_SECRET, {expiresIn:"24h"});
+
+// };
+// module.exports={generateToken};
