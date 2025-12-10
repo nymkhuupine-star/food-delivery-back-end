e0be0a6bf24eb6c73a9816d858f6b27303019616
25LP5105 25LP5105 <25LP5105@G302-CC0181.local>
2025-12-10 12:20:03 +0800

    unndr

--- controllers/food/getFoodByCategoryId.js
+++ controllers/food/getFoodByCategoryId.js
@@ -1,14 +1,15 @@
 const Food = require("../../schemas/food");
 
-const getFoodById = async (req, res) => {
+const getFoodByCategoryId = async (req, res) => {
   try {
     const { id } = req.params;
-    const food = await Food.findById(id);
+    const food = await Food.find({ category: id });
     if (!food) return res.status(404).json({ msg: "Food not found" });
+
     return res.json(food);
   } catch (err) {
     res.status(500).json({ msg: err.message });
   }
 };
 
-module.exports = getFoodById;
+module.exports = getFoodByCategoryId;
--- routes/foodRouter.js
+++ routes/foodRouter.js
@@ -3,12 +3,15 @@ const createFood = require("../controllers/food/createFood");
 const getFood = require("../controllers/food/getFood");
 const updateFood = require("../controllers/food/updateFood");
 const deleteFood = require("../controllers/food/deleteFood");
-const getFoodById = require("../controllers/food/getFoodById");
+const getFoodByCategoryId = require("../controllers/food/getFoodByCategoryId");
 
 const foodRouter = express.Router();
-foodRouter.get("/", getFood);
+
 foodRouter.post("/", createFood);
-foodRouter.get("/:id", getFoodById);
+
+foodRouter.get("/", getFood);
+foodRouter.get("/category/:id", getFoodByCategoryId);
+
 foodRouter.put("/:id", updateFood);
 foodRouter.delete("/:id", deleteFood);
 
